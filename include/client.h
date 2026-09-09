63e899f7e388cebe030f837caf9fe5913aed62ee
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-09 18:49:40 +0500

    Add files via upload

---
+++ include/client.h
@@ -0,0 +1,18 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Example public API. Replace with your own real functions.
+ */
+int client_get_version(void);
+const char* client_get_name(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CLIENT_H
